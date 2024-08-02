f297a0ac3b18869054137721b9fa07703153707f
cubidev <cubidev.vitu@gmail.com>
2024-08-01 21:54:19 -0300

    Fixed some errors

--- Assets/_Scripts/Components/KnockbackComponent.cs
+++ Assets/_Scripts/Components/KnockbackComponent.cs
@@ -3,13 +3,17 @@ using UnityEngine.Events;
 
 public class KnockbackComponent : MonoBehaviour
 {
+    [SerializeField] private CharacterBody2D body;
     [SerializeField] private DamageComponent damageComponent;
-    [SerializeField] private float baseKnockback;
-    public UnityEvent<float> onKnockback;
     
-    public void ApplyKnockback()
+    public UnityEvent onKnockback;
+    
+    public void ApplyKnockback(Vector2 knockback)
     {
-        float knockbackForce = baseKnockback * damageComponent.currentDamage;
-        onKnockback.Invoke(knockbackForce);
+        knockback *= KnockbackMultiplier(damageComponent.CurrentDamage);
+        body.ApplyImpulse(knockback);
+        onKnockback.Invoke();
     }
+
+    float KnockbackMultiplier(float damage) => 1.0f + damage * 0.05f;
 }
