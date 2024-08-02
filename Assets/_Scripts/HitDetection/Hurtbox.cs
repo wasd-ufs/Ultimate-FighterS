3a6c1ace096b88ae3c09b385015779d30e477322
cubidev <cubidev.vitu@gmail.com>
2024-08-01 22:23:35 -0300

    Removed unnecessary IsInvincible and fixed a small error

--- Assets/_Scripts/HitDetection/Hurtbox.cs
+++ Assets/_Scripts/HitDetection/Hurtbox.cs
@@ -5,18 +5,15 @@ using UnityEngine.Events;
 public class Hurtbox : MonoBehaviour
 {
     [SerializeField] private GameObject owner;
-    [SerializeField] private bool isInvincible;
-
     [SerializeField] private UnityEvent onHitBoxDetected;
+    
     public GameObject Owner => owner;
-    public bool IsInvincible => isInvincible;
 
     public void OnHurted(GameObject _owner)
     {
         if (owner == _owner)
-        {
-            onHitBoxDetected.Invoke();
-        }
-        return;
+            return;
+        
+        onHitBoxDetected.Invoke();
     }
 }
