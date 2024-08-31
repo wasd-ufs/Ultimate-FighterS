89bd2574c71b17f09ac0ecc488eeb87104da1274
Cubidev3 <perdigaomaisamigo@gmail.com>
2024-08-31 08:44:04 -0300

    Lots of little changes

--- Assets/_Scripts/Components/IdComponent.cs
+++ Assets/_Scripts/Components/IdComponent.cs
@@ -5,4 +5,22 @@ using UnityEngine;
 public class IdComponent : MonoBehaviour
 {
     public int id;
+    
+    public static IdComponent GetFirstIdInHierarchy(GameObject baseObj)
+    {
+        if (baseObj == null)
+            return null;
+
+        while (true)
+        {
+            var id = baseObj.GetComponent<IdComponent>();
+            if (id != null)
+                return id;
+
+            if (baseObj.transform == baseObj.transform.root)
+                return null;
+            
+            baseObj = baseObj.transform.parent.gameObject;
+        }
+    }
 }
---
+++ Assets/_Scripts/Components/OwnedComponent.cs
@@ -0,0 +1,18 @@
+using System;
+using UnityEngine;
+
+public abstract class OwnedComponent : MonoBehaviour
+{
+    private IdComponent id;
+
+    public int Id => id?.id ?? -1;
+    public GameObject Owner => id?.gameObject ?? transform.root.gameObject;
+
+    private void Start()
+    {
+        id = IdComponent.GetFirstIdInHierarchy(gameObject);
+    }
+
+    public bool IsSameAs(OwnedComponent component)
+     => Id == component.Id;
+}
