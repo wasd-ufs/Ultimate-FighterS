bb1cec93ca45bb73b92c2ec94c45c777610a14de
Wallisson Jesus <wallisson_wjc@outlook.com.br>
2024-09-08 17:57:23 -0300

    building a scene

--- Assets/GameFolder/GamePhases/Phase1/Scripts/Parallax/Parallax.cs
+++ Assets/GameFolder/GamePhases/Phase1/Scripts/Parallax/Parallax.cs
@@ -1,18 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Collections;
 using UnityEngine;
 
 public class Parallax : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private Transform cam;
+    private float spritelength;
+    private Vector2 startPos;
+    public float speedParallaxEfx;
+
     void Start()
     {
-        
+        startPos = transform.position;
+        spritelength = GetComponent<SpriteRenderer>().bounds.size.x;
     }
 
-    // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        
+        //0 = Dont move & 1 = Move with cam
+        Vector2 distance = cam.position * speedParallaxEfx;
+
+        //Move
+        transform.position = new Vector3(startPos.x + distance.x, startPos.y + distance.y,transform.position.z);
     }
 }
