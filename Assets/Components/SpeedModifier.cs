ea39e959f02968d8d5ea22cbb1524899f1a47d49
josevic01 <162633306+josevic01@users.noreply.github.com>
2024-10-04 13:05:21 -0300

    add 10% velocity

---
+++ Assets/Components/SpeedModifier.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeedModifier : OwnedComponent
+{
+
+public void SpeedModified (GameObject target) {
+    Rigidbody OwnerRigid = Owner.GetComponent<Rigidbody>();
+    Rigidbody targetRigid = target.GetComponent<Rigidbody>();
+
+    if (OwnerRigid && targetRigid) {        
+        Vector3 Speed10Porcent = OwnerRigid.velocity * 0.1f;
+
+        targetRigid.velocity += Speed10Porcent;
+    }
+}
+}
