91a1178f96f8b61c6cb3a66b90139e226f7eb55e
Wallisson Jesus <wallisson_wjc@outlook.com.br>
2024-11-01 23:47:02 -0300

    Basic logic rotate in hitlag

--- Assets/_Scripts/Character/States/Behaviours/Physics/RotateBehaviour.cs
+++ Assets/_Scripts/Character/States/Behaviours/Physics/RotateBehaviour.cs
@@ -6,8 +6,12 @@ public class RotateBehaviour : CharacterState
 
     public override void PhysicsProcess()
     {
-        var direction = input.GetDirection().x;
-        direction = direction < -0.01f ? 1f : direction > 0.01f ? -1f : 0f;
+        var direction = input.GetDirection();
+        
+        float angle = Mathf.Atan2(direction.y,direction.x) * Mathf.Rad2Deg;
+        body.RotateVelocity(angle);
+        
+        direction.x = direction.x < -0.01f ? 1f : direction.x > 0.01f ? -1f : 0f;
         body.RotateVelocity(direction * speed * Time.fixedDeltaTime);
     }
 }
