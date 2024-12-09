0984ad0058b2d4107da587b5e0b008f64aaed13b
Cubidev <cubidev.vitu@gmail.com>
2024-12-09 01:43:48 -0300

    fixed camera

--- Assets/_Scripts/Camera/CameraController.cs
+++ Assets/_Scripts/Camera/CameraController.cs
@@ -1,14 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
     public float minZoom = 5f;
     public float maxZoom = 17f;
-    public float zoomSpeed = 5f;
-    public float moveSpeed = 3f;
+    public float zoomSpeed = 0.75f;
+    public float moveSpeed = 0.5f;
     public float padding = 2f;
 
     private Camera cam;
@@ -41,7 +42,7 @@ public class CameraController : MonoBehaviour
         }
         
         maxZoom = Mathf.Min((worldMaxBounds.x - worldMinBounds.x) / cam.aspect, worldMaxBounds.y - worldMinBounds.y) / 2f;
-        Debug.Log(maxZoom);
+        cam.orthographicSize = maxZoom;
     }
 
     void LateUpdate()
@@ -61,16 +62,16 @@ public class CameraController : MonoBehaviour
         {
             averagePosition += position;
         }
-        
         averagePosition /= players.Count;
-        Vector3 finalPosition = Vector3.Lerp(transform.position, averagePosition, Time.deltaTime * moveSpeed);
-                
+        
         float camHalfHeight = cam.orthographicSize;
         float camHalfWidth = camHalfHeight * cam.aspect;
-
+        
+        Vector3 finalPosition = Vector3.Lerp(transform.position, averagePosition, 1 - Mathf.Exp(-Time.deltaTime * moveSpeed));
+        
         finalPosition.x = Mathf.Clamp(finalPosition.x, worldMinBounds.x + camHalfWidth, worldMaxBounds.x - camHalfWidth);
         finalPosition.y = Mathf.Clamp(finalPosition.y, worldMinBounds.y + camHalfHeight, worldMaxBounds.y - camHalfHeight);
-
+        
         transform.position = new Vector3(finalPosition.x, finalPosition.y, transform.position.z);
     }
 
@@ -79,9 +80,12 @@ public class CameraController : MonoBehaviour
         var players = GetAlivePlayersPositions();
         if (players.Count == 0)
             return;
+
+        var bounding = BoundingZoom(players, padding, cam.aspect);
+        var desiredZoom = Mathf.Clamp(bounding, 0f, maxZoom);
         
-        var desiredZoom = BoundingZoom(players, padding, cam.aspect);
-        cam.orthographicSize = Mathf.Clamp(Mathf.Lerp(cam.orthographicSize, desiredZoom, Time.deltaTime * zoomSpeed), 0f, maxZoom);
+        Debug.Log($"Bounding: {bounding}, desired: {desiredZoom}, min: {minZoom}, max: {maxZoom}");
+        cam.orthographicSize = Mathf.Lerp(cam.orthographicSize, desiredZoom, 1 - Mathf.Exp(-Time.deltaTime * zoomSpeed));
     }
 
     private List<Vector2> GetAlivePlayersPositions() => MatchManager.GetActivePlayers()
@@ -90,19 +94,19 @@ public class CameraController : MonoBehaviour
     
     private static float BoundingZoom(List<Vector2> points, float padding, float aspect)
     {
-        float maxX = points.Max(p => p.x) + padding;
-        float maxY = points.Max(p => p.y) + padding;
-        float minX = points.Min(p => p.x) - padding;
-        float minY = points.Min(p => p.y) - padding;
+        float maxX = points.Max(p => p.x);
+        float maxY = points.Max(p => p.y);
+        float minX = points.Min(p => p.x);
+        float minY = points.Min(p => p.y);
 
         return Zoom(maxX, maxY, minX, minY, padding, aspect);
     }
     
     private static float Zoom(float maxX, float maxY, float minX, float minY, float padding, float aspect)
     {
-        float width = maxX - minX + padding;
-        float height = maxY - minY + padding;
+        float halfWidth = (maxX - minX) / 2f + padding;
+        float halfHeight = (maxY - minY) / 2f + padding;
 
-        return Mathf.Max(width / aspect, height) / 2f;
+        return Mathf.Max(halfWidth / aspect, halfHeight);
     }
 }
