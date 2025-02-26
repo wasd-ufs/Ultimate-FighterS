613ffe3f37698aeaf46e3f63230f86a5a10a9e20
wallisson de jesus campos <wallisson_wjc@outlook.com.br>
2025-02-26 18:41:34 -0300

    refactor: Adcionando comentario fase gato
    Adicionando padrao de comentario e codigo para a fase do gato

--- Assets/UltimateFighterS/GamePhases/Phase1/Scripts/Event/CatCenter.cs
+++ Assets/UltimateFighterS/GamePhases/Phase1/Scripts/Event/CatCenter.cs
@@ -2,9 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Classe responsavel por executar a acao do centro do gato
+/// </summary>
 public class CatCenter : CatBaseMoves
 {
-
     [SerializeField]private Animator _animator;
     [SerializeField]private GameObject _skin;
 
@@ -13,12 +15,24 @@ public class CatCenter : CatBaseMoves
         _skin.SetActive(false);  
     }
 
+
+    /// <summary>
+    /// Responsavel por executar o ataque do centro
+    /// </summary>
+    /// <return>void</return>
+    /// <author>Wallisson de jesus</author>
     public override void Execute()
     {
         _skin.SetActive(true);
         _animator.Play("CenterCat",-1);
     }
 
+
+    /// <summary>
+    /// Responsavel por desabilitar o ataque do centro
+    /// </summary>
+    /// <return>void</return>
+    /// <author>Wallisson de jesus</author>
     public override void Hide()
     {
         _skin.SetActive(false);
--- Assets/UltimateFighterS/GamePhases/Phase1/Scripts/Event/CatLeft.cs
+++ Assets/UltimateFighterS/GamePhases/Phase1/Scripts/Event/CatLeft.cs
@@ -2,6 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Classe responsavel por executar a acao a esquerda do gato
+/// </summary>
 public class CatLeft : CatBaseMoves
 {
     [SerializeField] private Animator _animator;
@@ -12,12 +15,23 @@ public class CatLeft : CatBaseMoves
         _skin.SetActive(false);
     }
 
+    /// <summary>
+    /// Responsavel por executar o ataque da esquerda
+    /// </summary>
+    /// <return>void</return>
+    /// <author>Wallisson de jesus</author>
     public override void Execute()
     {
         _skin.SetActive(true);
         _animator.Play("CatLeftPunch",-1);
     }
 
+
+    /// <summary>
+    /// Responsavel por desabilitar o ataque da esquerda
+    /// </summary>
+    /// <return>void</return>
+    /// <author>Wallisson de jesus</author>
     public override void Hide()
     {
         _skin.SetActive(false);
