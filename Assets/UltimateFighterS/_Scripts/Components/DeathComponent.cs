79adf3b0726a7db497b59857924fdaf198050b17
joaoCarlosDev <joaocarlosprocarvalho@gmail.com>
2025-03-16 07:52:39 -0300

    padronização de codigo

--- Assets/UltimateFighterS/_Scripts/Components/DeathComponent.cs
+++ Assets/UltimateFighterS/_Scripts/Components/DeathComponent.cs
@@ -1,18 +1,27 @@
 ﻿using System;
 using UnityEngine;
 using UnityEngine.Events;
+
+///<summary>
+/// Componente responsavel por aplicar morte em um jogador.
+///</summary>
 public class DeathComponent : MonoBehaviour
 {
     [HideInInspector] public UnityEvent onDeath;
-
+    /// <summary>
+    /// Responsavel por matar e acionar o evento de morte do player.
+    /// </summary>
+    /// <author>JOÃO CARLOS</author>
     public void Kill()
     {
         onDeath.Invoke();
-        
         var player = MatchManager.GetPlayer(gameObject);
         player.Kill();
     }
-
+    /// <summary>
+    /// Remove todos os nao-persistentes ouvintes do evento onDeath.
+    /// </summary>
+    /// <author>JOÃO CARLOS</author>
     private void OnDestroy()
     {
         onDeath.RemoveAllListeners();
