f86cc7c908b30d56f0b1e240e39ca41526dd0364
Jose Gabriel <jgr280704@gmail.com>
2025-03-23 18:32:30 -0300

    Padronizacao final e remocao de arquivos idea e vscode

--- Assets/UltimateFighterS/_Scripts/Match/Assemblers/DefaultMatchAssembler.cs
+++ Assets/UltimateFighterS/_Scripts/Match/Assemblers/DefaultMatchAssembler.cs
@@ -16,33 +16,37 @@ public class DefaultMatchAssembler : MonoBehaviour
         
         MatchManager.StartMatch();
         
-        bool _success = AddPlayers();
-        if (!_success)
+        if (!CanAddPlayers())
             MatchManager.EndMatch();
+        else
+            AddPlayers();
         
         MatchManager.ForEachPlayer(player => player.Spawn());
         SceneChangerController.FadeIn();
     }
 
-    private bool AddPlayers()
+    private bool CanAddPlayers()
     {
-        List<Transform> _spawns = GetSpawnPoints();
-        if (_spawns.Count < MatchConfiguration.Characters.Count)
+        List<Transform> spawns = GetSpawnPoints();
+        if (spawns.Count < MatchConfiguration.Characters.Count)
         {
-            Debug.LogError($"Not enough spawn points for all players! Point Count = {_spawns.Count}. Player Count = {MatchConfiguration.Characters.Count}");
+            Debug.LogError(
+                $"Not enough spawn points for all players! Point Count = {spawns.Count}. Player Count = {MatchConfiguration.Characters.Count}");
             return false;
         }
-
-        int _spawnPointIndex = 0;
-        foreach ((int _port, Character _character) in MatchConfiguration.Characters)
-        {
-            Transform _spawnPoint = _spawns[_spawnPointIndex];
-            MatchManager.AddPlayer(_port, MatchConfiguration.PlayerInputTypes[_port], _character, _spawnPoint);
-            
-            _spawnPointIndex++;
-        }
-        
         return true;
+    }
+    private void AddPlayers()
+    {
+        List<Transform> spawns = GetSpawnPoints();
+        int spawnPointIndex = 0;
+        foreach ((int port, Character character) in MatchConfiguration.Characters)
+        {
+            Transform spawnPoint = spawns[spawnPointIndex];
+            MatchManager.AddPlayer(port, MatchConfiguration.PlayerInputTypes[port], character, spawnPoint);
+            
+            spawnPointIndex++;
+        }
     }
     
     private List<Transform> GetSpawnPoints() => GameObject.FindGameObjectsWithTag(SpawnPointTag).ToList()
